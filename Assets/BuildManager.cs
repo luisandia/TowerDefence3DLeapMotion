7a1bd7649b60d81e6cf4fa019922322a2079f670
luisandia <luisandiamamani@gmail.com>
2018-11-02 01:38:23 -0500

    Creacion de torre standard

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	//singleton
+	public static BuildManager instance;
+
+	void Awake() {
+		if (instance != null){
+			Debug.LogError("More than one BuildManager in scene!");
+		}
+		instance = this;	
+	} 
+	public GameObject standartTurretPrefab;
+
+	private GameObject turretToBuild;
+
+	public GameObject GetTurretToBuild(){
+		return turretToBuild;
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		turretToBuild=standartTurretPrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    private GameObject turret;
+
+    private Color startColor;
+    private Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cant build there!, TODO: Display on screen.");
+            return;
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position+positionOffset, transform.rotation);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+
+
+
+}
